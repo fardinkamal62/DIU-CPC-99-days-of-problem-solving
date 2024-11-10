ea9dd6495569f880bcdf708935785b0397eb8e9b
Fardin Kamal <fardinkamal62@protonmail.ch>
2024-11-10 11:45:28 +0600

    day-10

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		"Day-7: Multiplication Dilemma (https://codeforces.com/problemset/gymProblem/101972/A)",
 		"Day-8: Cyclic Shift (https://codeforces.com/gym/101972/problem/K)",
 		"Day-9: Marathon (https://codeforces.com/problemset/problem/1692/A)",
+		"Day-10: Lucky? (https://codeforces.com/problemset/problem/1676/A)",
 	}
 
 	fmt.Println("Hello! Welcome to my solutions of DIU CPC 99 Days of Problem Solving")
